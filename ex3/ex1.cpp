ec2ce334ccbe497f8640048266d0cf3fc60730e1
vitaliyandr <116282877+vitaliyandr@users.noreply.github.com>
2023-05-07 18:58:43 +0300

    Add files via upload

---
+++ ex3/ex1.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+using namespace std;
+
+struct Rectangle {
+    int x;
+    int y;
+    int width;
+    int height;
+};
+
+void moveRectangle(Rectangle& rect, int dx, int dy) {
+    rect.x += dx;
+    rect.y += dy;
+}
+
+void resizeRectangle(Rectangle& rect, int dw, int dh) {
+    rect.width += dw;
+    rect.height += dh;
+}
+
+void printRectangle(Rectangle rect) {
+    cout << "x: " << rect.x << ", y: " << rect.y << ", width: " << rect.width << ", height: " << rect.height << endl;
+}
+
+int main() {
+    Rectangle rect = { 0, 0, 10, 5 };
+    moveRectangle(rect, 2, 3);
+    resizeRectangle(rect, 3, 2);
+    printRectangle(rect);
+
+    return 0;
+}
---
+++ ex3/ex2.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+struct Point {
+    int x;
+    int y;
+};
+
+double distance(Point p1, Point p2) {
+    int dx = p1.x - p2.x;
+    int dy = p1.y - p2.y;
+    return sqrt(dx * dx + dy * dy);
+}
+
+int main() {
+    Point p1 = { 0, 0 };
+    Point p2 = { 3, 4 };
+    double dist = distance(p1, p2);
+    cout << "Distance: " << dist << endl;
+
+    return 0;
+}
